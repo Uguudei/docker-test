6b90a3ceb8af645d6bb75f4e2387a11986cb32c4
Uguudei E <dev.uguudei@gmail.com>
2020-02-19 12:19:47 +0800

    Create helper.py

---
+++ src/module/helper.py
@@ -0,0 +1,42 @@
+import time
+
+
+def hello_world():
+    """This function is used for testing project structure"""
+    print('Hello World!')
+
+
+def elapsed_time(total_seconds):
+    """prints total elapsed time
+    Args:
+        total_seconds (float): total elapsed seconds
+    Returns:
+        total_elapsed_time (str): string of hours minutes seconds
+    """
+    days = int(total_seconds // 86400)
+    hours = int(total_seconds // 3600 - total_seconds // 86400 * 24)
+    minutes = int(total_seconds // 60 - total_seconds // 3600 * 60)
+    seconds = int(total_seconds - total_seconds // 60 * 60)
+    ms = int(total_seconds - total_seconds // 60 * 60)
+    elapsed_days = '{} d '.format(days) if days >= 1 else ''
+    elapsed_hours = '{} hr '.format(hours) if hours >= 1 else ''
+    elapsed_minutes = '{} min '.format(minutes) if minutes >= 1 else ''
+    elapsed_seconds = '{} sec '.format(seconds) if seconds >= 1 else ''
+    elapsed_ms = '{} ms'.format(ms) if ms > 0 else ''
+    return elapsed_days + elapsed_hours + elapsed_minutes + elapsed_seconds + elapsed_ms
+
+
+def timeit(method):
+    """method timer decorator"""
+    def timed(*args, **kw):
+        start_time = time.time()
+        result = method(*args, **kw)
+        end_time = time.time()
+        elapsed_duration = elapsed_time(time.time() - start_time)
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((end_time - start_time) * 1000)
+        else:
+            print('{!r:<20} time spent: {}'.format(method.__name__, elapsed_duration))
+        return result
+    return timed
