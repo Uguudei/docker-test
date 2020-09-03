424694df64e823bcb741cbed52bf793e50646b4b
Uguudei E <dev.uguudei@gmail.com>
2020-09-03 11:57:50 +0800

    update

--- script.py
+++ script.py
@@ -1,17 +1,19 @@
 import logging
-import sys
 
 from src.main import run
 
 logging.basicConfig(
     # filename='logs/basic.log',
-    format='%(asctime)-20s %(levelname)-8s L:%(lineno)-4d %(name)-30s M:%(module)-30s F:%(funcName)-20s | %(message)s',
+    format='%(asctime)-20s %(levelname)-8s L:%(lineno)-4d %(name)-32s M:%(module)-32s F:%(funcName)-20s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
+logger.info('start---------------------------------------------------------')
 
 if __name__ == '__main__':
     # run(sys.argv)
     run(['test', 'export'])
+
+logger.info('end-----------------------------------------------------------')
