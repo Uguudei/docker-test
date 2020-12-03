ce920c4bbcdff11eda0993a7b4a8c487528b3c38
Uguudei E <dev.uguudei@gmail.com>
2020-12-03 16:32:52 +0800

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import logging
 from src.main import run
 
 logging.basicConfig(
-    # filename='logs/basic.log',
+    filename='logs/basic.log',
     format='%(asctime)-20s %(levelname)-8s L:%(lineno)-4d %(name)-32s M:%(module)-32s F:%(funcName)-20s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.DEBUG
@@ -15,5 +15,6 @@ logger.info('start---------------------------------------------------------')
 if __name__ == '__main__':
     # run(sys.argv)
     run(['test', 'export'])
+    logger.info('volume working')
 
 logger.info('end-----------------------------------------------------------')
